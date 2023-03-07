05ac0976db634bc6b970c39a18fb1f10bf096c3d
EstebanSM <estsan93@outlook.com>
2023-03-07 01:28:43 +0000

    first rest with django completed

--- blog/settings.py
+++ blog/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://my-first-rest-django.herokuapp.com/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
